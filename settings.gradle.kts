f6728a109123979bca5888535154773d97eaa87a
andrei.badulescu01 <andrei.badulescu01@stud.acs.upb.ro>
2024-04-09 12:15:57 +0300

    first_commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PractivalTest01Var04"
+include(":app")
+ 
